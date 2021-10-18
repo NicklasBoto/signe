9ffd9e944dd70593122a8e5626d94b5381578865
Nicklas Botö <nicklas.boto@gmail.com>
2021-10-18 23:03:16 +0200

    Add SAST language converter

--- src/Frontend/SAST/Abs.hs
+++ src/Frontend/SAST/Abs.hs
@@ -1,31 +1,35 @@
 module Frontend.SAST.Abs where
 
 import Data.Complex ( Complex )
+import qualified Data.Map as Map
 
 type Program = [Toplevel]
 
-data Toplevel
-    = ToplF String [Arg] Expr 
-    | ToplFT String [Arg] Type Expr
+data Id 
+    = Id
+    { pos  :: (Int, Int)
+    , name :: String
+    } deriving (Eq, Show, Read, Ord)
+
+data Toplevel = Topl Id [[Id]] (Maybe Type) Expr
   deriving (Eq, Show, Read)
 
-type Arg = [(String, Type)]
+type Let = Map.Map [Id] Expr
 
 data Expr
-    = Var String
+    = Var Id
     | KetZero
     | KetOne
     | Tup [Expr]
     | Mul (Complex Double) Expr
     | App Expr Expr
     | Plus Expr Expr
+    | Comp Expr Expr
     | Ifq Expr Expr Expr
     | If Expr Expr Expr
-    | Let [[(Pattern, Expr)]] Expr
+    | Let Let Expr
+    | Abs [Id] Expr
   deriving (Eq, Show, Read)
 
-data Pattern = PVar String | PTup String String
-  deriving (Eq, Ord, Show, Read)
-
-data Type = TypeQubit | TypeUnit | Type :* Type | Type :-> Type
+data Type = Forall [Id] Type | TypeVar Id | TypeQubit | TypeUnit | Type :* Type | Type :-> Type
   deriving (Eq, Ord, Show, Read)

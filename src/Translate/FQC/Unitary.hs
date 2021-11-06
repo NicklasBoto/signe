1ed7c84d2eda9233a521d6c4577ba3f1206cc840
Nicklas Botö <nicklas.boto@gmail.com>
2021-11-06 23:08:16 +0100

    Remodel unitary datatype

--- src/Translate/FQC/Unitary.hs
+++ src/Translate/FQC/Unitary.hs
@@ -1,8 +1,44 @@
-module Translate.FQC.Unitary where
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE PatternSynonyms    #-}
+{-# LANGUAGE DerivingVia        #-}
 
-import Data.Complex ( Complex )
+module Translate.FQC.Unitary
+    ( C(..)
+    , Unitary(..)
+    ) where
 
-type C = Complex Double
+import Prelude hiding ( (<>) )
+import Data.Data ( Data )
+import Text.PrettyPrint
+    ( (<+>),
+      (<>),
+      braces,
+      brackets,
+      double,
+      int,
+      punctuate,
+      render,
+      sep,
+      text,
+      Doc )
+import qualified Data.Complex as Cx ( Complex(..) )
+
+newtype C = C (Cx.Complex Double)
+    deriving (Num, Fractional, Floating, Eq)
+        via (Cx.Complex Double)
+    deriving Data
+
+instance Show C where
+    show = render . showC
+
+showC :: C -> Doc
+showC (C (r Cx.:+ 0)) = double r 
+showC (C (0 Cx.:+ i)) = double i <> "i"
+showC (C (r Cx.:+ i)) = double r <+> "+" <+> double i <> "i"
+
+pattern (:+) :: Double -> Double -> C
+pattern (:+) r i = C (r Cx.:+ i)
 
 data Unitary 
     = Par [Unitary]
@@ -10,5 +46,16 @@ data Unitary
     | Perm [Int]
     | Cond Unitary Unitary
     | Rot (C,C) (C,C)
-    deriving (Eq, Show)
-    
+    deriving Eq
+
+instance Show Unitary where
+    show = render . showU
+
+showU :: Unitary -> Doc
+showU (Par   us) = sep $ punctuate "⊗" $ map showU us
+showU (Ser   us) = sep $ punctuate "⊙" $ map showU us
+showU (Perm  is) = braces $ sep $ map int is
+showU (Cond t c) = showU t <+> "if" <+> showU c
+showU (Rot (i,j) (k,l))
+    = brackets $ sep $ mid "|" $ map (text . show) [i,j,k,l]
+    where mid x xs = let (a,b) = splitAt 2 xs in a ++ x : b

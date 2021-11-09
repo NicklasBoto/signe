dcb74ff51f230645a14aab2d9e0fea5f7d945638
Fabian Forslund <fabfors@student.chalmers.se>
2021-11-09 17:02:48 +0100

    matrix testing

--- src/Translate/Compile/Compile.hs
+++ src/Translate/Compile/Compile.hs
@@ -3,5 +3,19 @@
 
 module Translate.Compile.Compile where
 
+import Frontend.SAST.Abs
+import Translate.FQC.FQC
+import Data.Map
+
+data Comp = Comp { usedContext :: [String]
+                 , types :: Type
+                 , fqc   :: FQC }
+
+data FSig = FSig Con Type
+data FDef = FDef FSig Expr
+type Env a = Map String a
+type Prog = Env FDef
+type Con = Env Type
+
 compile :: *
 compile = undefined

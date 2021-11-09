e086ae3dbf8e3e075cac1a4a5cedaa939345af18
Nicklas Botö <nicklas.boto@gmail.com>
2021-11-09 01:26:34 +0100

    Complete permutation functions

--- src/Translate/FQC/Matrix.hs
+++ src/Translate/FQC/Matrix.hs
@@ -4,46 +4,40 @@
 module Translate.FQC.Matrix where
 
 import Prelude hiding ( (<>) )
-import Translate.FQC.Unitary
+import Translate.FQC.Unitary ( Unitary(..), arity, complex )
 import Numeric.LinearAlgebra ( (><), (<>) )
 import qualified Numeric.LinearAlgebra as L
 import Control.Lens ( (.~), element )
-import Control.Monad ( guard )
 import Data.Function ( on )
-import Debug.Trace
-
-debug :: String -> Maybe ()
-debug = flip trace (return ())
+import Control.Monad ( guard, join, replicateM )
+import Data.List ( transpose )
 
 type Matrix = L.Matrix L.C
 
 instance {-# OVERLAPS #-} Show Matrix where
     show = L.dispcf 3
 
+-- add more checks
 matrix :: Unitary -> Maybe Matrix
 matrix (Par  xs) = foldl L.kronecker (L.ident 1) <$> mapM matrix xs
 matrix (Ser  xs) = foldl (<>) (L.ident 2) <$> mapM matrix xs
-matrix (Perm ps) = return $ permutationMatrix ps
+matrix (Perm ps) = return $ permutationMatrix $ scalePermutation ps
 matrix (Cond t c) = (guard =<< ((==) <$> arity t <*> arity c)) >> (<>) <$> matrix t <*> matrix c
 matrix (Rot (i,j) (k,l)) = return $ (2><2) $ map complex [i,j,k,l]
 
--- permutationMatrix :: [Int] -> Matrix
-permutationMatrix ps = scl . (d><d) $ concatMap (($ replicate d 0) . (.~ 1) . element) ps
-    where d     = length ps
-          scl x = x
+permutationMatrix :: [Int] -> Matrix
+permutationMatrix = join (on (><) length) 
+                <*> ( (=<<) 
+                  =<< flip ((.~ 1) . element) 
+                    . flip replicate 0 
+                    . length
+                    )
 
-exponentialDimension :: Int -> Matrix
-exponentialDimension 0 = (2><1) [1,0]
-exponentialDimension 1 = (2><1) [0,1]
-exponentialDimension _ = undefined
-
--- π  = {1 0 2}
--- ψ  = ~010 --> ~100
--- ψ  = [0 0 1 0 0 0 0 0] --> [0 0 0 1 0 0 0 0]
--- π* = {0 1 3 2 4 5 6 7} 
-
-
-
--- replaceAt = (.~ 1) . element
--- zeros = replicate d 0
--- e n = replaceAt n $ zeros
+scalePermutation :: [Int] -> [Int]
+scalePermutation = map (foldl ((+) . (2*)) 0)
+                 . transpose 
+                 . ( map =<< (!!) 
+                   . transpose 
+                   . flip replicateM [0,1] 
+                   . length
+                   )

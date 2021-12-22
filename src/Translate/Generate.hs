5a2476754fb4e328e0ea01c2088fb069e806c0b9
NicklasBoto <nicklas.boto@gmail.com>
2021-12-22 12:33:59 +0100

    Add OpenQASM backend
    Co-authored-by: Fabian Forslund <fabfors@users.noreply.github.com>

--- src/Translate/Generate.hs
+++ src/Translate/Generate.hs
@@ -4,41 +4,88 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE IncoherentInstances #-}
 
 module Translate.Generate where
 
-import Translate.Unitary.Data
+import Translate.Unitary
+import Translate.Result
 import Translate.FQC
 import GHC.TypeLits
 import Control.Monad.Writer
 import Control.Monad.State
 import Data.Bifunctor
+import qualified Numeric.LinearAlgebra as L
 import Data.List
+import Translate.Result
+import qualified Data.Complex as C
+import Data.Complex (conjugate)
 
--- Ser [ Par [Perm [0,1]]
---     , Perm [0,1]
---     , Par [Perm [0],Perm [0]]
---     , Perm [0,1]
---     , Par [Cond (Rot (0.0,1.0) (1.0,0.0)) (Perm [0])]
---     , Perm [1,0]
---     ]
+test :: Unitary
+test = Ser [ Par [Perm [0,1]]
+    , Perm [0,1]
+    , Par [Perm [0],Perm [0]]
+    , Perm [0,1]
+    , Par [Cond (Rot (0.0,1.0) (1.0,0.0)) (Perm [0])]
+    , Perm [1,0]
+    ]
 
-newtype Gen a = Gen { getGen :: StateT (Int,Int) (Writer QASM) a }
-    deriving (Functor, Applicative, Monad, MonadState (Int,Int), MonadWriter QASM)
+tf :: FQC
+tf = FQC
+    { input = 1
+    , heap = 1
+    , output = 2
+    , garbage = 0
+    , unitary = test
+    }
+
+type Circ = [Gate]
+
+newtype Gen a = Gen { getGen :: StateT (Int,Int) (Writer Circ) a }
+    deriving (Functor, Applicative, Monad, MonadState (Int,Int), MonadWriter Circ)
+
+-- runGen :: Gen a -> a
+-- runGen m = runStateT (StateT (0,0) m) ()
+runGen :: Gen a -> ((a, (Int, Int)), Circ)
+runGen = runWriter . flip runStateT (0,0) . getGen
+
+evalGen :: Gen a -> Circ
+evalGen = snd . runGen
+
+generate :: FQC -> QASM
+generate (FQC i h o g φ) = QASM
+    { qubits = i+h
+    , discard = g
+    , circuit = evalGen (genCircuit φ)
+    }
+
+putQASM :: FQC -> String
+putQASM = putCircuit . generate
 
 data QASM
     = QASM
-    { row :: Int
-    , col :: Int
+    { qubits :: Int
     , discard :: Int
-    , circuit :: [[Gate]]
-    }
+    , circuit :: Circ
+    } deriving Show
 
 data Gate
-    = Unitary C C C
-    | Control Gate
+    = Unitary Int C C C
+    | CX Int Int
     | Swap Int Int
     deriving Show
+
+x, h :: Int -> Gate
+x q = Unitary q pi 0 (-pi)
+h q = Unitary q (pi/2) 0 (-pi)
+
+{-
+qubit heap[3];
+
+U(θ,φ,λ) heap[0];
+
+swap heap[0], heap[1];
+-}
 
 instance Eq Gate where
     Swap a b == Swap c d = (a,b) == (c,d) || (a,b) == (d,c)
@@ -49,23 +96,75 @@ instance Semigroup QASM where
 
 instance Monoid QASM where
     mappend = (<>)
-    mempty = QASM 0 0 0 []
+    mempty = QASM 0 0 []
 
 wire :: Gate -> Gen ()
-wire g = modify (first succ)
+wire = tell . pure
+
+wires :: [Gate] -> Gen ()
+wires = tell
+
+addRow, addCol :: Int -> Gen ()
+addRow = modify . first . (+)
+addCol = modify . second . (+)
+
+incRow, incCol, decRow, decCol :: Gen ()
+incRow = addRow 1
+incCol = addCol 1
+decRow = addRow (-1)
+decCol = addCol (-1)
+
+modSwap :: Int -> Gate -> Gate
+modSwap i ~(Swap x y) = Swap (x+i) (y+i) 
 
 genCircuit :: Unitary -> Gen ()
-genCircuit (Rot (i,j) (k,l)) = undefined -- qasmGate i j k l
-genCircuit (Cond φ ψ) = undefined
-genCircuit (Perm ps) = tell $ permutationSwaps ps
+genCircuit (Rot (i,j) (k,l)) = do
+    (r,_c) <- get
+    let (θ,φ,λ) = qasmGate i j k l
+        gate    = Unitary r θ φ λ
+    wire gate
+genCircuit (Cond (Rot (i,j) (k,l)) (Rot (a,b) (c,d))) = do
+    incRow
+    (r,_c) <- get
+    let ((θ,φ,λ), (θ',φ',λ')) = controlledGate i j k l
+        ((t,f,l), (t',f',l')) = controlledGate a b c d
+        gates = [ Unitary r θ φ λ, CX (r-1) r, Unitary r θ' φ' λ', CX (r-1) r
+                , x (r-1), Unitary r t f l, CX (r-1) r, Unitary r t' f' l', CX (r-1) r, x (r-1)
+                ]
+    tell gates
+genCircuit (Cond (Rot (i,j) (k,l)) (Perm [0])) = do
+    incRow
+    (r,_c) <- get
+    let ((θ,φ,λ), (θ',φ',λ')) = controlledGate i j k l
+        gates = [ Unitary r θ φ λ, CX (r-1) r, Unitary r θ' φ' λ', CX (r-1) r ]
+    tell gates
+    decRow
+genCircuit (Perm ps) = do
+    (r,_c) <- get 
+    let swaps = map (modSwap r) $ permutationSwaps ps
+    tell swaps 
+genCircuit (Par ps) = mapM_ (addRow. testResult . arity >> genCircuit) ps
+genCircuit (Ser ss) = mapM_ (addCol . testResult . arity >> genCircuit) ss
 genCircuit _ = undefined
 
 permutationSwaps :: [Int] -> [Gate]
 permutationSwaps = filter (\(Swap m n) -> m /= n) . nub . zipWith Swap [0..]
 
-qasmGate :: C -> C -> C -> C-> Gate
-qasmGate i j k l = Unitary θ φ λ
+controlledGate :: C -> C -> C -> C -> ((C,C,C), (C,C,C))
+controlledGate i j k l = ((θ,φ,λ), (θ',φ',λ'))
+    where φ1 = i
+          conj = C . C.conjugate . complex
+          m  = L.matFunc sqrt ((2 L.>< 2) (crotations (i,j) (k,l)))
+          md = L.tr m
+          [[mi,mj],[mk,ml]] = L.toLists m
+          [[mdi,mdj],[mdk,mdl]] = L.toLists md
+          (θ,φ,λ) = qasmGate (C mi) (C mj) (C mk) (C ml)
+          (θ',φ',λ') = qasmGate (C mdi) (C mdj) (C mdk) (C mdl)
+
+qasmGate :: C -> C -> C -> C -> (C,C,C)
+qasmGate i j k l = (real θ, real φ, real λ)
     where imag = 0 :+ 1
+          real = C . (C.:+ 0) . C.realPart . complex
           θ = 2 * acos i
           φ = if j == 0 then 0 else log (j / sin (θ/2)) / imag
           λ = if k == 0 then 0 else log (-(k / sin (θ/2))) / imag
@@ -75,11 +174,28 @@ qasmGate i j k l = Unitary θ φ λ
 -- }
 
 qasmString :: [[String]] -> String
-qasmString = unlines . map unwords
+qasmString = unlines . map concat
 
-putGate :: Gate -> String -> String
-putGate ~(Unitary θ φ λ) name = unlines $ map unwords
-    [[ "gate", name, "unary_qubit", "{" ]
-    ,[ "    U(", show θ, ",", show φ, ",", show λ, ") unary_qubit ;" ]
-    ,[ "}" ]
+putCircuit :: QASM -> String
+putCircuit (QASM q d circ) = qasmString
+    [ [ "// GENERATED BY THE SIGNE COMPILER" ]
+    , [ "// Nicklas Botö, Fabian Forslund 2021" ]
+    , [ "OPENQASM 2.0;" ]
+    , [ "include \"qelib1.inc\";" ]
+    , [ "" ]
+    , [ "// initialise qubit registers" ]
+    , [ "qreg heap[", show q, "];" ]
+    , [ "creg output[", show (q-d), "];" ]
+    , [ "" ]
+    , [ "// main program" ]
+    , map ((++"\n") . putGate) circ
+    , [ "" ]
+    , [ "// prepare output" ]
+    , map (\i -> "reset heap[" ++ show i ++ "];\n") [q-d..q-1]
+    , map (\i -> "measure heap[" ++ show i ++ "] -> output[" ++ show i ++ "];\n") [0..q-d-1]
     ]
+
+putGate :: Gate -> String
+putGate (Unitary q θ φ λ) = concat ["U(", show θ, ",", show φ, ",", show λ, ") heap[", show q, "];"]
+putGate (CX p q) = concat ["CX heap[", show p, "], heap[", show q, "];"]
+putGate (Swap m n) = concat ["swap heap[", show m, "], heap[", show n, "];"]
